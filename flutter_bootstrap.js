1c5ba57c37deb608c1aaac557cd11a64423d3797
jgravena <jgravena@users.noreply.github.com>
2026-04-27 23:21:43 -0300

    Multi-perfil local + Lv 7-8 + pratos endgame

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2108476573" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2407387371" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
